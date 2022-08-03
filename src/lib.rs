b09a2b55b7d9acf23693d063decfed8ca57b7d9f
David Tolnay <dtolnay@gmail.com>
2022-08-03 06:39:50 -0700

    Release 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@
 //! ![performance](https://raw.githubusercontent.com/dtolnay/dragonbox/master/performance.png)
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/dragonbox/0.1.2")]
+#![doc(html_root_url = "https://docs.rs/dragonbox/0.1.3")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
